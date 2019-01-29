b58504a2bf11bd133945c02d70c377bb8bbd7781
Michelle Rauert <32675078+Rauert@users.noreply.github.com>
2019-01-30 07:43:14 +0800

    Day 11 Partial

--- AOC2016/Day11.py
+++ AOC2016/Day11.py
@@ -1,5 +1,29 @@
+import itertools
 
-def Part12():
+best = 100000000
+fin = []
+goal = 4
+
+def recurseRun(count, e, f):
+    if count > best: return
+    if len(f[4]) == goal:
+        fin.append(count)
+        return
+    count +=1
+    #Test every item and every combination for above floor
+    combs = list(itertools.combinations(f[e],2))
+    for j in f[e]: combs.append((j,))
+    print(combs)
+    #if e < 4:
+    #    for c in combs:
+    #        if canMove(c,f[e+1]):
+
+    ##Test every item and every combination for below floor
+    #if e > 1:
+    #    for c in combs:
+    #        if canMove(c,f[e+1]):
+
+def Part1():
     f = {4:[],
          3:["OG","OM","RG","RM"],
          2:["PM","SM"],
@@ -11,7 +35,8 @@ def Part12():
          1:["HM","LM"]}
     e = 1
     count = 0
+    recurseRun(count,e,f)
 
-    print("Part 1:", count)
+    print("Part 1:", min(fin))
 
-Part12()
+Part1()
